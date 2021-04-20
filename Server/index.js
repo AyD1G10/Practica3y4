1aaf741b656a92e038a2c9cf7e4fdb96103eff04
David Tórtola <davidtortola_@hotmail.com>
2021-04-19 21:48:40 -0600

    Peticion Post Login
    Se crea el endpoint para realizar una peticion post y validar el inicio de sesion de un usuario. Se recibe un json de la estructura:
    {
    	usuario:""
    	password:""
    }
    
    y se devuelve:
    
    {
    	respuesta:true,
    	mensaje:"",
    	id:0
    	nombre:""
    	apellido:""
    }

--- Server/index.js
+++ Server/index.js
@@ -2,16 +2,44 @@
 
 const express = require('express')
 const cors = require('cors');
+const login = require('../Server/login.js');
 const app = express()
 const port = 3000
 
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('¡BlockBuster!')
 })
 
 app.post('/Login', function (request, response) {
+  if (!request || !response) {
+    response.send(JSON.stringify({ msg: false }));
+  }
   console.log(request.body);
-  return response.send(JSON.stringify({ msg: true }));
+
+  if (login.Validar(request.body)) {
+    return response.send(
+      JSON.stringify({
+        respuesta: true,
+        mensaje: "",
+        id: 1,
+        nombre: "nombre",
+        apellido: "apellido"
+      })
+    );
+  } else {
+    return response.send(
+      JSON.stringify({
+        respuesta: false,
+        mensaje: "Usuario no existe",
+        id: -1,
+        nombre: "",
+        apellido: ""
+      })
+    );
+  }
 })
 
 app.listen(port, () => {
---
+++ Server/login.js
@@ -0,0 +1,11 @@
+
+
+function Validar(request) {
+    let user = request['usuario'];
+    let password = request['password'];
+
+    return true;
+
+}
+
+module.exports = { Validar }
