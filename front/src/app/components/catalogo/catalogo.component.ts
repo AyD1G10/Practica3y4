90193841dee76a32b78bd0ea3c3cab5f08846be0
Josue Perez <joshua72.perez@gmail.com>
2021-05-02 23:40:12 -0600

    Creacion vista Catalogo
    Se creo la vista en el front end del catalogo de peliculas.

---
+++ front/src/app/components/catalogo/catalogo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { UserInterface } from 'src/app/models/user-inteface';
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrls: ['./catalogo.component.css']
+})
+export class CatalogoComponent implements OnInit {
+
+  resultados: any;
+
+  constructor(public auth: UserService,public router: Router) { }
+
+  ngOnInit(): void {
+    this.getCatalogo();
+  }
+  cerrarSesion() {
+    this.auth.logout();
+  }
+  getCatalogo() {
+      this.auth.getCatalogo().subscribe((res) => {
+       
+        this.resultados = res['list'];
+         console.log(this.resultados);
+      })
+    }
+}
