2ff417d222713e2020f96e948b03d1a1ae9c949f
Sandra Jimenez <sandraeujim@gmail.com>
2021-05-05 23:57:47 -0600

    Transaccion de pago
    Se realizaron los metodos para la transaccion del pago de alquiler de peliculas

--- front/src/app/components/alquiler-peliculas/alquiler-peliculas.component.ts
+++ front/src/app/components/alquiler-peliculas/alquiler-peliculas.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from "../../services/user.service";
-
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-alquiler-peliculas',
   templateUrl: './alquiler-peliculas.component.html',
@@ -8,12 +8,30 @@ import { UserService } from "../../services/user.service";
 })
 export class AlquilerPeliculasComponent implements OnInit {
 
-  constructor(public userService: UserService) { }
+  constructor(public userService: UserService, public router: Router) { }
 
   total : number = 0;
   plan : string = "";
   tarjeta : string = ""; 
-  
+  arreglo : any = JSON.parse(localStorage.getItem('carrito')!);
+  usuario : any = JSON.parse(localStorage.getItem('UsuarioLogueado')!);
+   arrP : Array<any> = new Array<any>();
+   arrE : Array<any> = new Array<any>();
+  selected = "----"
+  repetidos : Array<any> = new Array<any>();
+
+  update(e:any, item:any){
+    this.selected = e.target.value
+    console.log(this.selected);
+    console.log(item);
+    this.total += item.chargeRate * Number(this.selected);
+    this.arrE.push(item.chargeRate),
+    this.arrP.push(this.selected);
+  }
+  resetear(){
+    this.total = 0;
+    this.repetidos = [];
+  }
   cerrarSesion() {
     this.userService.logout();
   }
@@ -42,10 +60,42 @@ export class AlquilerPeliculasComponent implements OnInit {
           }
       ]
   }
-    localStorage.setItem('carrito', JSON.stringify(user_string));
-    this.alquiler_pelicula();
+    //localStorage.setItem('carrito', JSON.stringify(user_string));
+    console.log(this.arreglo);
+   // this.alquiler_pelicula();
+   // this.prueba();
   }
 
+  prueba(){
+    console.log(document.getElementById("select1")?.addEventListener("click", this.prueba2, true));
+  }
+
+  prueba2(){
+    console.log("true")
+  }
+
+  cancelar(){
+    this.router.navigate(['/catologo']);
+  }
+
+  transaccion(){
+    var llave = Date.now().toString().substr(0,4) + Math.random().toString(10).substr(5, 4);
+    let arrM : Array<any> = new Array<any>();
+    
+
+    for(let items of this.arreglo){
+      arrM.push(items.id);
+      
+    }
+
+    this.userService.realizar_pago(this.usuario._id, llave, arrM, this.arrP, this.arrE, this.total)
+    .subscribe((res)=>{
+      console.log(res);
+      alert("Se realizo la transaccion correctamente.");
+    }
+    )
+    
+  }
 
   alquiler_pelicula():void {
     let arreglo_carrito : any = [];
@@ -58,10 +108,19 @@ export class AlquilerPeliculasComponent implements OnInit {
         var cuerpohtml = "<thead class=\"thead-dark\"><tr><th scope=\"col\">#</th><th scope=\"col\">Pelicula</th><th scope=\"col\">Precio</th><th scope=\"col\">Plan</th><th scope=\"col\">Lenguaje</th></tr></thead>";
         let cont = 1;
         cuerpohtml = cuerpohtml + "<tbody>";
-        for(let elem of obj.carrito){
+        let contplan = 1;
+        for(let elem of obj){
           cuerpohtml += "<tr><th scope=\"row\">" + cont + "</th><td>" + elem.name + 
-                "</td><td>" + elem.chargeRate +"</td>"  + 
-                "</td><td>" + elem.availabilities + 
+                "</td><td>" + elem.chargeRate +"</td>" +
+                
+                "</td><td>" + " <select class=\"custom-select\" id=\"select" + contplan+ " [onchange] = item\"><option selected>Seleccionar Plan...</option>";
+                
+                for(let val of elem.availabilities){
+                  cuerpohtml += "<option value=" + contplan +">" + val.name + "</option>"
+                  contplan++;
+                }
+                //"<option value=\"1\">One</option><option value=\"2\">Two</option><option value=\"3\">Three</option>
+                cuerpohtml +=  "</select>" + 
                 "</td><td>" + elem.languages + 
                 "</td>" +  "</tr>";
           cont++;
@@ -70,11 +129,16 @@ export class AlquilerPeliculasComponent implements OnInit {
         cuerpohtml = cuerpohtml + "</tbody><br><br>";
         cuerpohtml = cuerpohtml + "<table class=\"table\"><thead class=\"thead-light\"><tr><th scope=\"col\">Total</th><th scope=\"col\">" +this.total +"</th></tr></thead></table>"
         document.getElementById("tablacarrito")!.innerHTML = cuerpohtml; 
+        console.log(document.getElementById("inputGroupSelect01")?.textContent)
         console.log("total", this.total);
+
+
       }
    // })
 
 
     
   }
+
+  
 }
