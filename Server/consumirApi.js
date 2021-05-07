0314a7aac047d99a9ec779d6b64679d51de3a6b5
Josue Perez <joshua72.perez@gmail.com>
2021-05-07 15:10:48 -0600

    creacion del método para actualizar la bd de peliculas
    se creo el método para actualizar las películas en la bd según la información en la api.

--- Server/consumirApi.js
+++ Server/consumirApi.js
@@ -69,12 +69,12 @@ function getApiAvialabity() {
             });
             //console.log(availability);
             availability.save()
-            .then(result => {
-                console.log(result);
-            })
-            .catch(err => console.log(err));
+                .then(result => {
+                    console.log(result);
+                })
+                .catch(err => console.log(err));
         }
-        
+
     });
 }
 
@@ -96,18 +96,18 @@ function getLenguage() {
             });
             //console.log(language);
             language.save()
-            .then(result => {
-                console.log(result);
-            })
-            .catch(err => console.log(err));
+                .then(result => {
+                    console.log(result);
+                })
+                .catch(err => console.log(err));
         }
 
-        
+
     });
 }
 
-const getExchangeRate = async function ()  {
-    
+const getExchangeRate = async function () {
+
     const tasadeCambio = await ExchangeRate.find();
 
     var dato_api;
@@ -120,7 +120,7 @@ const getExchangeRate = async function ()  {
         for (let dato of json) {
             dato_api = dato.total;
             mongoose.set('useFindAndModify', false);
-            await  ExchangeRate.findOneAndUpdate(
+            await ExchangeRate.findOneAndUpdate(
                 {
                     _id: tasadeCambio[0]._id
                 },
@@ -133,8 +133,85 @@ const getExchangeRate = async function ()  {
     });
 }
 
+const updateMovies = async function () {
+    Request.get("https://my-json-server.typicode.com/CoffeePaw/AyD1API/Movie", async (error, response, body) => {
+        if (error) {
+            return console.dir(error);
+        }
+        var json = JSON.parse(body);
+        for (let dato of json) {
+            const movie = await Catalogo.find({ name: dato.name })
+
+            if (movie[0] != undefined) {
+                mongoose.set('useFindAndModify', false);
+                await Catalogo.findOneAndUpdate(
+                    {
+                        name: dato.name
+                    },
+                    {
+                        exchangeRate: dato.chargeRate,
+                        active: dato.active,
+                    });
+            } else {
+                //agregar pelicula
+                var available = []
+                for (let item of dato.availabilities) {
+                    available.push(item);
+                }
+                var lenguajes = []
+                for (let item of dato.languages) {
+                    lenguajes.push(item);
+                }
+
+                const movie = new Catalogo({
+                    _id: new mongoose.Types.ObjectId(),
+                    id: dato.id,
+                    name: dato.name,
+                    image: dato.image,
+                    exchangeRate: dato.chargeRate,
+                    active: dato.active,
+                    availabilities: available,
+                    languages: lenguajes
+                });
+
+                movie.save()
+                    .then(result => {
+                        console.log(result);
+                    })
+                    .catch(err => console.log(err));
+            }
+        }
+
+        //actualizar para no mostrar 
+        const movie = await Catalogo.find();
+        var bandera = false;
+        for (let valor of movie) {
+            // console.log(movie);
+            for (let dato of json) {
+                if (valor.name == dato.name) {
+                    //console.log("lo encontro " + valor.name);
+                    bandera = true;
+                }
+            }
+            if (bandera == false) {
+                //console.log("desactivas " + valor.name);
+                
+                await Catalogo.findOneAndUpdate(
+                    {
+                        name: valor.name
+                    },
+                    {
+                        active: false,
+                    });
+            }
+            bandera = false;
+        }
+    });
+}
+
 //getLenguage();
 //getApiMovies();
 //getApiAvialabity();
 //getExchangeRate();
-module.exports = { getExchangeRate , getLenguage , getApiAvialabity , getApiMovies , getExchangeRate} 
+//updateMovies();
+module.exports = { getExchangeRate, getLenguage, getApiAvialabity, getApiMovies, getExchangeRate, updateMovies }
